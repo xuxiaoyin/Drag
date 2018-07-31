0838524e78d5d211713172a74c1ea1f89212aaf7
xuxiaoyin <279238850@qq.com>
2018-07-31 10:20:48 +0800

    change

--- drag.js
+++ drag.js
@@ -1,3 +1,24 @@
-function(id){
+function Drag(id){
+	var obj=document.getElementById(id);
+	var disx=0;
+	var disy=0;
 	
+	obj.onmousedown=function(ev){
+		var ev=ev||window.event;
+		disx=ev.clientX-obj.offsetLeft;
+		disy=ev.clientY-obj.offsetTop;
+		
+		document.onmousemove=function(ev){
+			var ev=ev||window.event;
+			obj.style.left=ev.clientX-disx+'px';
+			obj.style.top=ev.clientY-disy+'px';
+		}
+		
+		document.onmouseup=function(){
+			document.onmousemove=null;
+			document.onmouseup=null;
+		}
+		
+		return false;
+	}
 }
